c4675084527b3b75b37b399489f43ecd42e34dd0
Christie Beukman <me.mcbeukman@gmail.com>
2017-06-16 23:55:10 +0200

    Working

---
+++ Own/Command/CommandSample/CommandSample/CommandSampleViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CommandSample
+{
+    public class CommandSampleViewModel
+    {
+        public RelayCommand theCommand { get; private set; }
+
+        public CommandSampleViewModel()
+        {
+            theCommand = new RelayCommand(OnExecute, CanExecute);
+        }
+
+        bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        void OnExecute()
+        {
+            MessageBox.Show("Command Executed");
+        }
+    }
+}
---
+++ Own/Command/CommandSample/CommandSample/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CommandSample
+{
+    public class RelayCommand : ICommand
+    {
+        private Action _executeMethod;
+        private Predicate<object> _canExecuteMethod;
+
+        public RelayCommand(Action executeMethod, Predicate<object> canExecuteMethod)
+        {
+            _executeMethod = executeMethod;
+            _canExecuteMethod = canExecuteMethod;
+        }
+
+        #region ICommand Members
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecuteMethod == null)
+                return true;
+
+            return _canExecuteMethod(parameter);
+        }
+        public void Execute(object parameter)
+        {
+            _executeMethod();
+        }
+        #endregion
+    }
+}
